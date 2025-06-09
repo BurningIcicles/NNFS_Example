e3f4354fcfe73a75086a47cd7557dc35f12ab59c
Richies Huynh <89647964+BurningIcicles@users.noreply.github.com>
2025-06-08 21:33:22 -0700

    Init

---
+++ main.py
@@ -0,0 +1,44 @@
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.output = None
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU:
+    def __init__(self):
+        self.output = None
+
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+class Activation_Softmax:
+    def __init__(self):
+        self.output = None
+
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+nnfs.init()
+X, y = spiral_data(100, 3)
+dense1 = Layer_Dense(2, 3)
+activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+output = activation2.output
+print(output[:5])
