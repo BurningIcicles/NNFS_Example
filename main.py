3d54e328882c47cf8af5db9b40c0718371624e22
Richies Huynh <89647964+BurningIcicles@users.noreply.github.com>
2025-06-11 20:38:10 -0700

    Basic optimizer for linear data

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import nnfs
-from nnfs.datasets import spiral_data
+from nnfs.datasets import vertical_data
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -27,18 +27,65 @@ class Activation_Softmax:
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
 
+class Loss:
+    def __init__(self):
+        self.output = None
+
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        else:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+
+        negative_log_likelihood = -np.log(correct_confidences)
+        return negative_log_likelihood
+
 nnfs.init()
-X, y = spiral_data(100, 3)
+X, y = vertical_data(100, 3)
 dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
 
 dense2 = Layer_Dense(3, 3)
 activation2 = Activation_Softmax()
 
-dense1.forward(X)
-activation1.forward(dense1.output)
-dense2.forward(activation1.output)
-activation2.forward(dense2.output)
+loss_function = Loss_CategoricalCrossentropy()
 
-output = activation2.output
-print(output[:5])
+lowest_loss = 999999
+best_dense1_weights = dense1.weights.copy()
+best_dense1_biases = dense1.biases.copy()
+best_dense2_weights = dense2.weights.copy()
+best_dense2_biases = dense2.biases.copy()
+learning_rate = 0.1
+
+
+for iteration in range(1):
+    dense1.weights += 0.05 * np.random.randn(2, 3)
+    dense1.biases += 0.05 * np.random.randn(1, 3)
+    dense2.weights += 0.05 * np.random.randn(3, 3)
+    dense2.biases += 0.05 * np.random.randn(1, 3)
+
+    dense1.forward(X)
+    print("Weights and Biases of Layer 1: ", dense1.output)
+    activation1.forward(dense1.output)
+    dense2.forward(activation1.output)
+    activation2.forward(dense2.output)
+
+    loss = loss_function.calculate(activation2.output, y)
+    # predictions = np.argmax(activation2.output, axis=1)
+    # accuracy = np.mean(predictions == y)
+
+    dZ2 = activation2.output - y
+    dW2 = dense1.output.T @ dZ2 / X.shape[0]
+    db2 = np.sum(dZ2, axis=1, keepdims=True) / X.shape[0]
+
+print("Finished training")
